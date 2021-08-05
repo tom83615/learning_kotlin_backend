0777810bcee7c8c41ec084fc24ab2b817e46a2cd
Hsieh,YuHsuan <tom83615@gmail.com>
2021-08-06 00:33:01 +0800

    Hello kotlin will be return!

--- src/main/kotlin/com/example/test/TestApplication.kt
+++ src/main/kotlin/com/example/test/TestApplication.kt
@@ -2,10 +2,20 @@ package com.example.test
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class TestApplication
 
 fun main(args: Array<String>) {
     runApplication<TestApplication>(*args)
+}
+
+@RestController
+class IndexController(){
+    @GetMapping("/")
+    fun getHelloString(): String{
+        return "Hello Kotlin!"
+    }
 }
