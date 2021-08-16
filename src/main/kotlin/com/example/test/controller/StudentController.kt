622fb82bd334d322225601e339f810d09831f8f8
Hsieh,YuHsuan <tom83615@gmail.com>
2021-08-17 00:08:10 +0800

    Add URI command for StudentController

--- src/main/kotlin/com/example/test/controller/StudentController.kt
+++ src/main/kotlin/com/example/test/controller/StudentController.kt
@@ -7,36 +7,42 @@ import org.springframework.web.bind.annotation.*
 
 interface StudentController {
     /**
+     * (GET:/students)
      * 取得 Student 所有資料
      */
     @GetMapping("/students")
     fun getStudentData(): MutableList<Student>
 
     /**
+     * (POST:/students)
      * 新增 Student 資料
      */
     @PostMapping("/students")
     fun addStudentData(@RequestBody student: Student) : Student
 
     /**
+     * (POST:/students/access?{id})
      * 利用 id 查詢學生資料
      */
     @PostMapping("/students/access")
     fun getStudentById(@RequestParam id: Int) : ResponseEntity<Student?>
 
     /**
+     * (POST:/students/search?{name})
      * 利用 name 查詢學生資料
      */
     @PostMapping("/students/search")
     fun getStudentByName(@RequestParam name: String) : ResponseEntity<List<Student>>
 
     /**
+     * (PUT:/students/{id})
      * 修改學生資料
      */
     @PutMapping("/students/{id}")
     fun updateStudent(@PathVariable id: Int, @RequestBody student: Student) : ResponseEntity<Student?>
 
     /**
+     * (DELETE:/students/{id})
      * 刪除學生資料
      */
     @DeleteMapping("/students/{id}")
