82912c980edfa469efd4b4476625bc73a4f32b07
Hsieh,YuHsuan <tom83615@gmail.com>
2021-08-24 00:23:45 +0800

    Remove not use code

--- src/main/kotlin/com/example/test/TestApplication.kt
+++ src/main/kotlin/com/example/test/TestApplication.kt
@@ -7,9 +7,6 @@ import org.springframework.core.env.Environment
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
-import java.sql.Connection
-import java.sql.DriverManager
-
 @SpringBootApplication
 class TestApplication
 
@@ -19,16 +16,9 @@ fun main(args: Array<String>) {
 
 @Controller
 class IndexController(@Autowired environment: Environment){
-    // 取得 application.yml 設定的配置數值
-    private final val url = environment.getProperty("spring.datasource.url");
-    private final val username = environment.getProperty("spring.datasource.username");
-    private final val password = environment.getProperty("spring.datasource.password");
-
-    // 資料庫連線
-    val connection: Connection = DriverManager.getConnection(url, username, password)
 
     @RequestMapping("/")
-    fun getHelloString(): String{
+    fun getHelloHtml(): String{
         return "index"
     }
 }
